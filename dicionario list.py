a2d60bb59eb2ccf221fcdb9633c649e42e8662e7
emanuelfnd <Emanuel_f_costa@estudamte.sesisenai.org.br>
2025-02-13 15:38:01 -0300

    adicinei comentario

--- dicionario list.py
+++ dicionario list.py
@@ -1,9 +1,9 @@
-ra = {"liz" : 229874, "hugo": 215793, "sopia": 199745}
+ra = {"liz" : 229874, "hugo": 215793, "sopia": 199745} #dados principais
 print (list(ra.items()))
 #dict_items({('liz', 229874), ('hugo', 215793), ('sofia', 199745)})resposta
 
 print (list(ra.keys()))
 #dict_keys(['liz', 'hugo', 'sofia']) resposta
 
-print (list(ra.values()))
+print (list(ra.values())) 
 #dict_values([229874, 215793, 199745]) resposta
